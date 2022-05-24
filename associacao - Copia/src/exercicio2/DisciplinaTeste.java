debe9e3961e03b7e80ff6e607c7196e9ada5203b
Gustavo Vicente Ozorio <gustavoozorio1227@hotmail.com>
2022-05-23 22:30:41 -0300

    Renomeação

--- associacao - Copia/src/exercicio2/DisciplinaTeste.java
+++
@@ -1,9 +0,0 @@
-package exercicio2;
-
-public class DisciplinaTeste {
-	public static void main(String[] args) {
-		Disciplina disciplina1 = new Disciplina(15124, "Matematica", Modalidade.DISTANCIA, 25, 25874, "Francisco",
-				"17/02/2020", 958.25);
-		disciplina1.emitirRelatorio();
-	}
-}
